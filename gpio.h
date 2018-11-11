906672eabe918e8f2a969794a689248a815d03ea
nhenault <nahe4217@colorado.edu>
2018-11-10 17:01:42 -0700

    Added GPIO functions
    1. gpioConfiguration - Sets up P2.4, P2.5, and P2.6 to be outputs
         which control the red, green, and blue LEDs respectively.
    2. setLEDColor - Takes in the battery voltage ADC reading and sets
         the LED color based on what is read.
    3. Sets the LED color in the ADC power status reading routine.

--- gpio.h
+++ gpio.h
@@ -1,6 +1,11 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+#define LOW_VOLTAGE    11528  // ADC reading of 2.33V = Battery voltage 14V
+#define HIGH_VOLTAGE   12410  // ADC reading of 2.5V  = Battery voltage 15V
 
+void gpioConfiguration();
+
+void setLEDColor(uint16_t battV);
 
 #endif /* GPIO_H_ */
